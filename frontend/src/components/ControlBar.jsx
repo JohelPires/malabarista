2929e68ddc7dd41936aff4e5a089ac113c692dfa
JohelPires <johelpires@gmail.com>
2023-07-26 06:36:49 -0400

    controlbar categoria seleciona index

--- frontend/src/components/ControlBar.jsx
+++ frontend/src/components/ControlBar.jsx
@@ -1,12 +1,53 @@
 import React, { useState } from 'react'
-import { Button, Container, Form, Stack } from 'react-bootstrap'
+import { Button, Col, Container, Form, Row, Stack } from 'react-bootstrap'
 
 function ControlBar() {
     const [valor, setValor] = useState(0.0)
+    const [categ, setCateg] = useState(false)
+
+    const [despesas, setDespesas] = useState([
+        'Alimentação', // (restaurantes, supermercado, delivery)', //
+        'Moradia', // (aluguel, hipoteca, contas de serviços públicos)',
+        'Transporte', // (gasolina, transporte público, manutenção do veículo)',
+        'Saúde', // (seguro de saúde, medicamentos, consultas médicas)',
+        'Lazer', // (cinema, viagens, hobbies)',
+        'Vestuário', // (roupas, sapatos, acessórios)',
+        'Educação', // (mensalidades escolares, cursos, livros)',
+        'Contas de Serviços', // (internet, TV a cabo, telefone)',
+        'Dívidas', // (empréstimos, cartões de crédito)',
+        'Seguros', // (seguro de vida, seguro de carro)',
+        'Presentes e Doações',
+        'Impostos e Taxas',
+        'Gastos com Animais de Estimação',
+        'Cuidados Pessoais', // (salão de beleza, academia)',
+        'Outros', // (outras despesas não cobertas por categorias anteriores)',
+    ])
+
+    const [receitas, setReceitas] = useState([
+        'Salário e Remuneração',
+        'Renda Extra (freelance, trabalhos temporários)',
+        'Rendimentos de Investimentos (juros, dividendos)',
+        'Aluguel de Propriedades',
+        'Reembolsos',
+        'Vendas de Bens (venda de itens usados, por exemplo)',
+        'Pensões ou Benefícios Sociais',
+        'Prêmios e Bonificações',
+        'Receitas de Negócios Próprios (caso o usuário seja empreendedor)',
+        'Outros Rendimentos (quaisquer outras fontes de renda não mencionadas)',
+    ])
 
     function handleChange(e) {
         console.log(e.target.value)
         setValor(e.target.value)
+    }
+
+    function handleDespesa() {
+        setCateg(true)
+    }
+
+    function handleConfirma() {
+        console.log(categ)
+        setCateg(false)
     }
 
     return (
@@ -22,7 +63,7 @@ function ControlBar() {
                     type='text'
                     className='transaction-input'
                 ></Form.Control>
-                <Button variant='success' size='sm'>
+                <Button variant='success' size='sm' onClick={handleDespesa}>
                     Despesa
                 </Button>
                 <Button variant='success' size='sm'>
@@ -30,15 +71,30 @@ function ControlBar() {
                 </Button>
                 <h3 className='ms-auto'>Saldo: R$ 4.125,00</h3>
             </Stack>
-            <Stack direction='horizontal' gap={2}>
-                <Form.Select size='sm' aria-label='Default select example'>
-                    <option>Categoria</option>
-                    <option value='1'>One</option>
-                    <option value='2'>Two</option>
-                    <option value='3'>Three</option>
-                </Form.Select>
-                <Button size='sm'>Confirmar</Button>
-            </Stack>
+            {categ && (
+                <Container>
+                    <Row>
+                        <Col md={5} className='mt-2'>
+                            <Stack direction='horizontal' gap={2}>
+                                <Form.Select
+                                    onChange={(e) => setCateg(e.target.value)}
+                                    size='sm'
+                                    aria-label='Default select example'
+                                >
+                                    <option>Selecione a categoria</option>
+                                    {despesas.map((d, i) => {
+                                        return <option value={i}>{d}</option>
+                                    })}
+                                </Form.Select>
+                                <Button onClick={handleConfirma} size='sm'>
+                                    Confirmar
+                                </Button>
+                            </Stack>
+                        </Col>
+                        <Col></Col>
+                    </Row>
+                </Container>
+            )}
         </Stack>
     )
 }
